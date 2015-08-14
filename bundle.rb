fa938e60ba73e3156735c91357e3448885e8a314
Francesco Canessa <makevoid@gmail.com>
2015-08-15 00:20:06 +0100

    remove comments

--- bundle.rb
+++ bundle.rb
@@ -31,30 +31,6 @@ module TxFetcher
 
           tx_viz.bids = (tx_viz.bids + bids).sort_by{ |price, volume| -price } unless bids.empty?
           tx_viz.asks = (tx_viz.asks + asks).sort_by{ |price, volume| price  } unless asks.empty?
-
-          # tx_viz.bids.push bids
-
-
-          # data  = `JSON.parse(e.native.data).x`
-          # out   = `data.out`
-          # hash  = `data.hash`
-          # value = out.map{ |o| `o.value` / 10 ** 8 }.inject :+
-          # value = value.round 8
-          # tx    = { value: value, hash: hash }
-          #
-          # comp_num = 100
-          # tx_gone = tx_viz.transactions[comp_num+1]
-          #
-          # if tx_gone
-          #   # TODO: look at ref
-          #   reactid = ".0.3.$#{tx_gone[:hash]}"
-          #   # React.findDOMNode
-          #   elem_gone = `document.querySelector("div[data-reactid='"+reactid+"']")`
-          #   `React.unmountComponentAtNode(elem_gone)`
-          # end
-          # # tx_viz.transactions.push tx
-          # tx_viz.transactions = [tx] + tx_viz.transactions[0..comp_num]
-          # tx_viz.total_value  = tx_viz.total_value + value
         end
       end
     end
