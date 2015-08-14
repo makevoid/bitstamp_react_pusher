db5481465b34e09d82080848b117f3007558511e
Francesco Canessa <makevoid@gmail.com>
2015-08-15 00:24:08 +0100

    add idea comment

--- bundle.rb
+++ bundle.rb
@@ -73,6 +73,19 @@ class TxViz
       end
       div className: "right_panel" do
         div className: "theme colors" do
+          # TODO: idea for a form components plugin
+          #
+          # string parse to html elements
+          #
+          # [] = checkbox (unchecked)
+          # [x] = checkbox (checked)
+          # () = radio buttons
+          # etc..
+          #
+          # api: el("string") - el "string" (something like that)
+          #
+          # the name its taken from the label, the collection name from the outher element
+          
           p { "theme colors" }
           p { "[  ] light" }
           p { "[ x ] color" }
